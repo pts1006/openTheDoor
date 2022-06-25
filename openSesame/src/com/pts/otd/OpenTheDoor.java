186297412225b9d00a8a277e18ee47b9e8b31211
admin <admin>
2022-06-25 22:57:03 +0900

    220625 22:56

---
+++ openSesame/src/com/pts/otd/OpenTheDoor.java
@@ -0,0 +1,23 @@
+package com.pts.otd;
+
+import java.util.Scanner;
+
+public class OpenTheDoor {
+
+	public static void main(String[] args) {
+		// 시건 장치
+
+		boolean doorOpen = false;
+
+		Scanner scanner = new Scanner(System.in);
+
+		int password = scanner.nextInt();
+
+		if (doorOpen == false) {
+
+		}
+
+		scanner.close();
+	}
+
+}
